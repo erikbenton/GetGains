00a133161c19090b610b04b1518235452ca91013
erikbenton <erik.benton@hotmail.com>
2022-05-02 15:20:44 -0700

    adding simple template for implementing CRUD for In Memory Exercises

---
+++ GetGains/GetGains.Data/Services/IExerciseData.cs
@@ -0,0 +1,16 @@
+﻿using GetGains.Core.Models.Exercises;
+
+namespace GetGains.Data.Services;
+
+public interface IExerciseData
+{
+    ICollection<Exercise> GetAll();
+
+    Exercise? GetById(int id);
+
+    Exercise Add(Exercise exercise);
+
+    Exercise? Update(Exercise exercise);
+
+    void Delete(Exercise exercise);
+}
---
+++ GetGains/GetGains.Data/Services/InMemExerciseData.cs
@@ -0,0 +1,41 @@
+﻿using GetGains.Core.Models.Exercises;
+
+namespace GetGains.Data.Services;
+
+public class InMemExerciseData : IExerciseData
+{
+    private readonly List<Exercise> _exercises;
+
+    public InMemExerciseData()
+    {
+        _exercises = new List<Exercise>()
+        {
+            new Exercise(),
+        };
+    }
+
+    public Exercise Add(Exercise exercise)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(Exercise exercise)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ICollection<Exercise> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Exercise? GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Exercise? Update(Exercise exercise)
+    {
+        throw new NotImplementedException();
+    }
+}
