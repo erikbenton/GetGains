e740178bf156aec24185aec1172d0c36676dec7a
erikbenton <erik.benton@hotmail.com>
2022-05-26 12:10:32 -0700

    adding foundation for API

---
+++ GetGains/GetGains.API/Controllers/ExerciseController.cs
@@ -0,0 +1,33 @@
+﻿using GetGains.Data.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GetGains.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExerciseController : ControllerBase
+    {
+        private readonly IExerciseData exerciseContext;
+
+        public ExerciseController(IExerciseData exerciseContext)
+        {
+            this.exerciseContext = exerciseContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var exercises = exerciseContext.GetAll();
+            return Ok("Got all");
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var exercise = exerciseContext.GetById(id);
+
+            return Ok($"Got id {id}");
+        }
+    }
+}
---
+++ GetGains/GetGains.API/Program.cs
@@ -0,0 +1,28 @@
+using GetGains.Data.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSingleton<IExerciseData>(new InMemExerciseData());
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
